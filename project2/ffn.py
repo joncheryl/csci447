51717304babd451e888f2dffb1f0bb79e773e6be
joncheryl <prof.sherrill@gmail.com>
2015-10-10 00:55:39 -0600

    update ff net that includes momentum and linear activation function and sigmoidal activation function capabilities

--- project2/ffn.py
+++ project2/ffn.py
@@ -1,5 +1,5 @@
 '''
-still need to get biases to learn
+Feedforward Neural Network
 '''
 
 import numpy as np
@@ -8,37 +8,52 @@ import math
 
 from operator import sub # for testing
 
-def sigmoid(x):
-     if x>0:
-         return 1 / (1 + math.exp(-x))
-     else:
-         return math.exp(x) / (math.exp(x) + 1)
+def actFun(x, act):
+    if act == 0: # Use sigmoid activation function
+        if x>0:
+            return 1 / (1 + math.exp(-x))
+        else:
+            return math.exp(x) / (math.exp(x) + 1)
+    else: # Use linear activation function
+        return slope * x
 
-def dsigmoid(x):
-    return math.exp(-x)/(1+math.exp(-x))**2
-    
+def dActFun(x, act):
+    if act == 0: # Use sigmoid activation function
+        return math.exp(-x)/(1+math.exp(-x))**2
+    else: # Use linear activation function
+        return slope
+     
 class ffNetwork:
 
-    def __init__(self, sizes, learningRate):
+     def __init__(self, sizes, learningRate, momentum = 0, act = 0):
         '''
         Each element in sizes contains the number of nodes in each
         respective layer. For example, if sizes is of length 3, sizes[0] would
         be how many input nodes are specified, sizes[1] is how many nodes are
         in the single hidden layer and sizes[2] is the number of output nodes
+
+        Momentum is disabled by default.
+
+        The default activation function is the sigmoid.
         '''
+
         self.sizes = sizes
         self.a = learningRate
-        
+        self.mom = momentum
+
         # Build network topology
+        # input layer
         self.nodes = [[inputNode(i) for i in range(sizes[0])]]
         self.edges = []
 
+        # hidden layers
         for layer in range(len(sizes))[1:-1]:
-            self.nodes.append( [Node() for j in range(sizes[layer])] )
+            self.nodes.append( [Node(act) for j in range(sizes[layer])] )
             for j in range(sizes[layer]):
                 for k in range(sizes[layer-1]):
                     self.connect(self.nodes[layer-1][k], self.nodes[layer][j], np.random.randn(1))
 
+        # output layer
         self.nodes.append([outputNode(i) for i in range(sizes[-1])])
         for j in range(sizes[-1]):
             for k in range(sizes[-2]):
@@ -51,7 +66,6 @@ class ffNetwork:
         downstream.addInput(connector)
                     
     # input must be a vector of length sizes[0]
-    # could be a numpy (n,1) array?
     def feedforward(self, inputVector):
         self.output = [i.feedforward(inputVector) for i in self.nodes[-1]]
         return self.output
@@ -67,8 +81,18 @@ class ffNetwork:
 
         # Update weights
         for edge in self.edges:
-            # w = w -learningRate * dEdOutput * dOutputdInput * dInputdWeight
-            edge.weight -= edge.a * edge.down.delta * edge.up.output
+            #w = w - learningRate * (dEdOutput * dOutputdInput) * dInputdWeight
+            weightChange = edge.a * edge.down.delta * edge.up.output
+            edge.weight -= weightChange - edge.oldWeightChange * self.mom
+            edge.oldWeightChange = weightChange
+
+        # Update biases
+        for layer in range(len(self.sizes))[1:]:
+             for node in self.nodes[layer]:
+                  # w_b = w_b - learningRate * (dEdOutput * dOutputdInput) * 1
+                  biasChange = node.delta
+                  node.bias -= self.a * biasChange - node.oldBiasChange * self.mom
+                  node.oldBiasChange = biasChange
 
     def getWeights(self):
          weights=[]
@@ -83,7 +107,7 @@ class Edge:
         self.down = downstream
 
         self.weight = weight
-
+        self.oldWeightChange = 0
         '''
         having the learning rate defined here may potentially allow for
         different learning rates at different levels of the network. May be
@@ -96,16 +120,16 @@ class Edge:
                            
 class Node:
 
-    def __init__(self):
+    def __init__(self, act):
+        self.act = act
         self.netInput = 0
         self.output = 0
-        # weights may need to be stored in upstream node for learning
         self.inputs = []
         self.outputs = []
+        
         self.bias = np.random.randn(1)
-
+        self.oldBiasChange = 0
         self.delta = 0
-        self.expectedOutput = 0
         
     def addInput(self, edge):
         self.inputs.append(edge)
@@ -114,16 +138,11 @@ class Node:
         self.outputs.append(edge)
         
     def feedforward(self, inputVector):
-        '''
-        adding zero makes it work... I have no idea why. 
-        It's like self.bias calls np.random.randn(1) everytime or something
-        '''
-        
-        self.netInput = 0 # + self.bias
+        self.netInput = 0 + self.bias
         for i in self.inputs:
             self.netInput += i.getOutput(inputVector)
 
-        self.output = sigmoid(self.netInput)
+        self.output = actFun(self.netInput, self.act)
         return self.output
 
     def backProp(self, target):
@@ -134,7 +153,7 @@ class Node:
             self.delta += outEdge.down.backProp(target) * outEdge.weight
 
         # dOutputdInput
-        self.delta *= dsigmoid(self.netInput)
+        self.delta *= dActFun(self.netInput, self.act)
 
         return self.delta
 
@@ -160,15 +179,16 @@ class outputNode(Node):
         self.netInput = 0
         self.inputs = []
         self.output = 0
-        self.bias = np.random.randn(1)
 
+        self.bias = np.random.randn(1)
+        self.oldBiasChange = 0
         self.delta = 0
         
     def addInput(self, edge):
         self.inputs.append(edge)
 
     def feedforward(self, inputVector):
-        self.netInput = 0 # + self.bias
+        self.netInput = 0 + self.bias
         for i in self.inputs:
             self.netInput += i.getOutput(inputVector)
 
