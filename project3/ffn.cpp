cc6060231a5e45642702824771e7f5584d11ccbb
joncheryl <prof.sherrill@gmail.com>
2015-10-25 08:15:50 -0600

    why you make me message little weewee changes

--- project3/ffn.cpp
+++ project3/ffn.cpp
@@ -81,63 +81,71 @@ int main(int argc, char** argv)
     // net input and activation vectors
     field<vec> netInputs(nLayers);
     field<vec> activations(nLayers+1);
-
-    // input training data
-    mat D;
-    D.load("data.csv", csv_ascii);
-
-    mat targets;
-    targets.load("data.csv", csv_ascii);
-    targets = targets.cols(0, 1);
-
     // change stuff
     field<mat> deltaW(nLayers);
     field<mat> deltaB(nLayers);
     vec activation;
     vec y;
     vec netInput;
-	
-    int point;
-    for(point = 0; point < D.n_rows; point++)
-    {
-	// feedforward
-	activation = D.row(point).t();
-	activations(0) = activation;
 
-	// possible to remove some of these intermediate containers?
-	// i.e. do i need activation below
-	for(i=0; i<nLayers; i++)
-	{
-	    netInput = W(i) * activation + B(i);
-	    netInputs(i) = netInput;
-	    activation = netInput.transform([](double val) { return actFun(val); });
-	    activations(i+1) = activation;
-	}
-
-	// backprop it
-	// get target value
-	y = targets.row(point).t();
-
-	// initialize backprop process
-	deltaB(nLayers - 1) = (activations(nLayers) - y) % netInputs(nLayers-1).transform([](double val) { return dActFun(val); });
-	deltaW(nLayers - 1) = deltaB(nLayers - 1) * activations(nLayers - 1).t();
-
-	// throw back errors
-	for(i = nLayers - 2; i >= 0; i--)
-	{
-	    deltaB(i) = W(i + 1).t() * deltaB(i + 1) % netInputs(i).transform([](double val) { return dActFun(val); });
-	    deltaW(i) = deltaB(i) * activations(i).t();
-	}
+    // input training data
+    mat D;
+    D.load("data.csv", csv_ascii);
+    mat targets;
+    targets.load("data.csv", csv_ascii);
+    targets = targets.cols(0, 1);
     
-	// adjust with learning rate
-	for(i = 0; i < nLayers; i++)
-	{
-	    deltaW(i).transform([=](double val) { return lRate * val; });
-	    deltaB(i).transform([=](double val) { return lRate * val; });
+    // index to training data
+    nPoints = D.n_rows;
+    std::vector<int> index;
+    for(int i = 0; i < nPoints; i++) index.push_back(i);
 
-	    // can we get away with -= shorthand here?
-	    W(i) = W(i) - deltaW(i);
-	    B(i) = B(i) - deltaB(i);
+    for(int epoch = 0; epoch < nEpochs; epoch++)
+    {
+	// randomize order of training points in each epoch
+	random_shuffle(index.begin(), index.end());
+	
+	for(int point = 0; point < nPoints; point++)
+	{
+	    // feedforward
+	    activation = D.row(index[point]).t();
+	    activations(0) = activation;
+
+	    // possible to remove some of these intermediate containers?
+	    // i.e. do i need activation below
+	    for(i=0; i<nLayers; i++)
+	    {
+		netInput = W(i) * activation + B(i);
+		netInputs(i) = netInput;
+		activation = netInput.transform([](double val) { return actFun(val); });
+		activations(i+1) = activation;
+	    }
+
+	    // backprop it
+	    // get target value
+	    y = targets.row(index[point]).t();
+
+	    // initialize backprop process
+	    deltaB(nLayers - 1) = (activations(nLayers) - y) % netInputs(nLayers-1).transform([](double val) { return dActFun(val); });
+	    deltaW(nLayers - 1) = deltaB(nLayers - 1) * activations(nLayers - 1).t();
+
+	    // throw back errors
+	    for(i = nLayers - 2; i >= 0; i--)
+	    {
+		deltaB(i) = W(i + 1).t() * deltaB(i + 1) % netInputs(i).transform([](double val) { return dActFun(val); });
+		deltaW(i) = deltaB(i) * activations(i).t();
+	    }
+    
+	    // adjust with learning rate
+	    for(i = 0; i < nLayers; i++)
+	    {
+		deltaW(i).transform([=](double val) { return lRate * val; });
+		deltaB(i).transform([=](double val) { return lRate * val; });
+
+		// can we get away with -= shorthand here?
+		W(i) = W(i) - deltaW(i);
+		B(i) = B(i) - deltaB(i);
+	    }
 	}
     }
     
