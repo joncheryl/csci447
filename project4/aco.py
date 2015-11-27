2891e5439d95fa61304631fadb949d1a73f7a250
joncheryl <prof.sherrill@gmail.com>
2015-11-27 12:25:20 -0700

    start project 4 with ant colony optimization clustering thingy

---
+++ project4/aco.py
@@ -0,0 +1,14 @@
+'''
+Ant Colony Optimization clustering algorithm
+
+Implimentation of Fuzzy c-Means Model
+
+John Sherrill
+
+CSCI 447 Fall 2015
+'''
+
+import pandas as pd
+
+data = pd.read_csv("datasets/blah.csv").as_matrix()
+
